0e5905761c6ad7c0cd6c2f9533a1fa937769f60f
Jonas Stare <yownas@gmail.com>
2026-06-03 12:48:52 +0200

    Update python_embeded script

--- tools/create_python_embeded.sh
+++ tools/create_python_embeded.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 # Bash script to build a small python_embed for Windows.
-URL=https://www.python.org/ftp/python/3.13.13/python-3.13.13-embed-amd64.zip
-RESULT=python_embeded
+VERSION=3.13.13
+URL=https://www.python.org/ftp/python/$VERSION/python-$VERSION-embed-amd64.zip
+RESULT=python_embeded_$VERSION
 DL=dl
 [ -d $RESULT ] && echo "$RESULT already exists" && exit 1
 [ -d $DL ] || mkdir $DL
@@ -11,5 +12,5 @@ unzip -x $DL/embed.zip -d $RESULT
 rename 's/_pth/pth/' $RESULT/python*._pth
 echo "import site" >> $(ls $RESULT/python*.pth)
 curl -L -o $RESULT/get-pip.py https://bootstrap.pypa.io/get-pip.py
-(proton-call -p 10.0 -r $RESULT/python.exe $RESULT/get-pip.py)
-(proton-call -p 10.0 -r $RESULT/Scripts/pip.exe install wheel packaging pygit2 setuptools==80.9.0 cffi==2.0.0)
+(wine $RESULT/python.exe $RESULT/get-pip.py)
+(wine $RESULT/Scripts/pip.exe install wheel packaging pygit2 setuptools==80.9.0 cffi==2.0.0)
